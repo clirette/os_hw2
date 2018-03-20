66817aef3e7b153f34e33c1369cdbf2a0129ae7a
Chase Lirette <chaselirette@yahoo.com>
2018-03-20 00:44:23 -0500

    Added way to keep track of which thread is executing with trackers. Helpful debugging print statements are helpful.

--- Bathrooms.java
+++ Bathrooms.java
@@ -1,11 +1,28 @@
+import java.util.Random;
+import java.util.concurrent.Semaphore;
 public class Bathrooms {
 	public static void main(String[] args) {
-
+		Random generator = new Random();
+		BathroomControl bsem = new BathroomControl();
 		Thread man = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				while (true) {
-
+					try {
+						int choice = generator.nextInt(2);
+						int tracker = generator.nextInt(1000);
+						//man wants to enter
+						if (choice == 0) {
+							System.out.println(tracker + ": trying man wants to enter");
+							bsem.manWantsToEnter(tracker);
+						} else if (choice == 1) {	//man leaves
+							System.out.println(tracker + ": trying man leaves");
+							bsem.manLeaves(tracker);
+						}
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		});
@@ -14,7 +31,20 @@ public class Bathrooms {
 			@Override
 			public void run() {
 				while (true) {
-
+					try {
+						int choice = generator.nextInt(2);
+						int tracker = generator.nextInt(1000);
+						if (choice == 0) {
+							System.out.println(tracker + ": trying woman wants to enter");
+							bsem.womanWantsToEnter(tracker);
+						} else if (choice == 1) {
+							System.out.println(tracker + ": trying woman leaves");
+							bsem.womanLeaves(tracker);
+						}
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		});
@@ -23,23 +53,71 @@ public class Bathrooms {
 		woman.start();
 	}
 
-	public static class Man {
-		public static void manWantsToEnter() {
+	public static class BathroomControl {
+		
+		private enum Status {EMPTY, WOMEN_PRESENT, MEN_PRESENT};
+		private static Status state;
+		private static int menInside;
+		private static int womenInside;
 
+		public BathroomControl() {
+			state = Status.EMPTY;
+			menInside = 0;
+			womenInside = 0;
 		}
 
-		public static void manLeaves() {
-			System.out.println("man leaves");
+		public void womanWantsToEnter(int tracker) {
+			if (state == Status.EMPTY) {
+				womenInside++;
+				state = Status.WOMEN_PRESENT;
+				System.out.println(tracker + ": woman enters, was empty " + "(" + womenInside + ")");
+			} else if (menInside > 0) {
+				//nothing
+				System.out.println(tracker + ": can't enter, men present " + "(" + menInside + ")");
+			} else if (womenInside > 0) {
+				womenInside++;
+				System.out.println(tracker + ": woman enters, only women " + "(" + womenInside + ")");
+			}
 		}
-	}
 
-	public static class Woman {
-		public static void womanWantsToEnter() {
-
+		public static void womanLeaves(int tracker) {
+			if (womenInside > 0) {
+				womenInside--;
+				System.out.println(tracker + ": woman leaves " + "(" + womenInside + ")");
+				if (womenInside == 0) {
+					state = Status.EMPTY;
+					System.out.println(tracker + ": now empty");
+				}
+			} else {
+				System.out.println(tracker + ": there were no women");
+			}
 		}
 
-		public static void womanLeaves() {
-			System.out.println("woman leaves");
+		public static void manWantsToEnter(int tracker) {
+			if (state == Status.EMPTY) {
+				menInside++;
+				state = Status.MEN_PRESENT;
+				System.out.println(tracker + ": man enters, was empty " + "(" + menInside + ")");
+			} else if (womenInside > 0) {
+				//nothing
+				System.out.println(tracker + ": can't enter, women present " + "(" + womenInside + ")");
+			} else if (menInside > 0) {
+				menInside++;
+				System.out.println(tracker + ": man enters, only men " + "(" + menInside + ")");
+			}
+		}
+
+		public static void manLeaves(int tracker) {
+			if (menInside > 0) {
+				menInside--;
+				System.out.println(tracker + ": man leaves " + "(" + menInside + ")");
+				if (menInside == 0) {
+					state = Status.EMPTY;
+					System.out.println(tracker + ": now empty");
+				}
+			} else {
+				System.out.println(tracker + ": there were no men");
+			}
 		}
 	}
 }
