fbfb86cf52fff5f88f926aca5a337d5d211dc4d7
Chase Lirette <chaselirette@yahoo.com>
2018-04-27 15:21:04 -0500

    started to implement multi-threaded stuff. may need to scrap this eventually.

--- matrix/MultiMatrix.java
+++ matrix/MultiMatrix.java
@@ -19,39 +19,33 @@ public class MultiMatrix {
 	public static void main(String[] args) {
 		int[][] arrayA = new int[100][100];
 		int[][] arrayB = new int[100][100];
-		int[][] result = new int[100][100];
 		
 		
 		// System.out.println("Array A:");
 		// printArray(arrayA);
 		// System.out.println("Array B:");
 		// printArray(arrayB);
-		ArrayList<Long> runtimes = new ArrayList<Long>();
-		for (int i=1; i<25; i++) {
-			
-			fillArrays(arrayA);
-			fillArrays(arrayB);
-			long start = System.nanoTime();
-			multiply(arrayA, arrayB, result);
-			long end = System.nanoTime() - start;
-			// System.out.println(""+start+", "+end);
-			System.out.println("runtime iteration " + i + " is\t" + end);
-			runtimes.add(end);
+		ArrayList<Thread> threads = new ArrayList<Thread>();
+		threads.add(new Thread())
+		int maxThreads = 100;
+		int offset = 10;
+		for (int threadcount=1; threadcount<maxThreads; threadcount+=offset) {
+			reportRuntimes(threads, arrayA, arrayB);
+			if (threadcount == 1) {
+				offset = 9;
+			}
+			for (int i=0; i<offset; i++) {
+				threads.add(new Thread());
+			}
 		}
 
-		long total = 0;
-		for (int i=0; i<runtimes.size(); i++) {
-			total += runtimes.get(i);
-		}
-		double average = total / ((double)runtimes.size());
-		System.out.println("average: " + average);
 
 		// System.out.println("Result:");
 		// printArray(result);
 
 	}
 
-	public static void fillArrays(int[][] array) {
+	public static void fillArray(int[][] array) {
 		Random random = new Random();
 		int outerLength = array.length;
 		int innerLength = array[0].length;
@@ -78,9 +72,33 @@ public class MultiMatrix {
 		}
 	}
 
-	public static void multiply(int[][] arrayA, int[][] arrayB, int[][] result) {
+	public static void reportRuntimes(ArrayList<Thread> threads, int[][] arrayA, int[][] arrayB) {
+		ArrayList<Long> runtimes = new ArrayList<Long>();
+		for (int i=1; i<25; i++) {	
+			fillArray(arrayA);
+			fillArray(arrayB);
+			for (Thread thread : threads) {
+				
+			}
+			long start = System.nanoTime();
+			int[][] result = multiply(arrayA, arrayB);
+			long end = System.nanoTime() - start;
+			// System.out.println(""+start+", "+end);
+			System.out.println("runtime iteration " + i + " is\t" + end);
+			runtimes.add(end);
+		}
+		long total = 0;
+		for (int i=0; i<runtimes.size(); i++) {
+			total += runtimes.get(i);
+		}
+		double average = total / ((double)runtimes.size());
+		System.out.println("average: " + average);
+	}
+
+	public static int[][] multiply(int[][] arrayA, int[][] arrayB) {
 		int outerLength = arrayA.length;
 		int innerLength = arrayA[0].length;
+		int[][] result = new int[outerLength][innerLength];
 
 		for (int i=0; i<outerLength; i++) {
 			for (int j=0; j<innerLength; j++) {
@@ -89,5 +107,6 @@ public class MultiMatrix {
 				}
 			}
 		}
+		return result;
 	}
 }
